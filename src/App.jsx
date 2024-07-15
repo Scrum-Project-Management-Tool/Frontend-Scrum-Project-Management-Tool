16f0c2621463ac7d17d057ec211f987e16593c20
Aashi19234 <aasthaj375@gmail.com>
2024-07-15 14:43:49 +0530

    Created the register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import {React, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Register from './components/register/Register'
+import {Router,Routes,Route} from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/register" Component={Register}/>
+        </Routes>
+    </Router>
+    
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Route,RouterProvider,createBrowserRouter,createRoutesFromElements} from 'react-router-dom'
+import Register from './components/register/Register.jsx'
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/register' element={<Register/>}/>
+    </Route>
+  )
+)
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>,
+
 )
